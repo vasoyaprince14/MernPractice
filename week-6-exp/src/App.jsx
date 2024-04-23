d6258f49bb36794b2e78d58f3360df2ade4e77bd
vasoyaprince14 <vasoyaprince14@gmail.com>
2024-04-23 22:49:58 +0530

    created a todo app which fetch request in every 10 sec

--- week-6-exp/src/App.jsx
+++ week-6-exp/src/App.jsx
@@ -1,26 +1,36 @@
-import { useState } from 'react'
+import { useState ,useEffect} from 'react'
 import './App.css'
 let id =4;
 function App() {
 
-  const [todos,setTodos]=useState([{
-    id:1,
-    title:"gym",
-    description:"go to gym"
-  },
-{
-  id:2,
-  title:"food",
-  description:"eat food by today"
-},
-{
-  id:3,
-  title:"bgmi",
-  description:"complete rank push"
-}])
-
+//   const [todos,setTodos]=useState([{
+//     id:1,
+//     title:"gym",
+//     description:"go to gym"
+//   },
+// {
+//   id:2,
+//   title:"food",
+//   description:"eat food by today"
+// },
+// {
+//   id:3,
+//   title:"bgmi",
+//   description:"complete rank push"
+// }])
+ const [todos,setTodos]=useState([]);
+ const [counter,setCounter]=useState(0);
+ useEffect(()=>{
+  setInterval(()=>{
+    fetch('https://sum-server.100xdevs.com/todos')
+  .then(async(res)=>{
+    const data=await res.json();
+   setTodos(data.todos);
+  });
+  },10000)
+},[])
 // console.log(todos);
-function addTodo(){
+ function addTodo(){
   
   setTodos([...todos,{
     id:id++,
