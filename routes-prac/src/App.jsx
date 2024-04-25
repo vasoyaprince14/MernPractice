5adade61f5814e3e4e57f671bf9776419ad6dce5
vasoyaprince14 <vasoyaprince14@gmail.com>
2024-04-25 22:58:13 +0530

    routes

--- routes-prac/src/App.jsx
+++ routes-prac/src/App.jsx
@@ -1,12 +1,11 @@
-import { useState } from 'react'
+// import { useState } from 'react'
 import { BrowserRouter,Routes,Route } from 'react-router-dom'
-import './App.css'
 import { Dashboard } from './components/Dashboard'
 import { Landing } from './components/Landing'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  // const [count, setCount] = useState(0)
+  
   return (
     <>
     <button onClick={()=>{
